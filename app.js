e2f4f1df0342fad0f4410b78b169eb5ad052782f
iamkryptonite <iamkryptonite@users.noreply.github.com>
2020-04-21 18:15:44 +0530

    included sidebar, info page for employees ,added a basic dashboard.css file and added new features in add new Employee form

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express       			=require('express'),
-    app           			=express();
+    app           			=express(),
+    bodyParser              =require('body-parser');
 var firebase                = require('firebase/app');
 require('firebase/auth');
 require('firebase/firestore');
@@ -8,6 +9,9 @@ require('firebase/firestore');
 //=========================================================================
 
 app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/public', express.static('public'));
+app.use('/assets', express.static('assets'));
 
 var firebaseConfig = {
     apiKey: "AIzaSyD4mrp7RWqRMv4QJB-LSR5ftzrpKTrTYyQ",
@@ -40,8 +44,46 @@ app.get("/",function(req,res){
     });
     // res.render("home");
 })
+//Adding employee to the employee collection
+app.get("/addEmployee",function(req,res){
+    // res.send("ssup");
+    res.render("addEmp");
 
+});
+app.post("/addEmployee",function(req,res){
+    var emp={
+        name: req.body.name,
+        Age: req.body.age,
+        designation: req.body.designation,
+        gender: req.body.gender
+        }
+        db.collection('employees').add(emp).then(ref=>{
+            res.redirect("/");
+        }); 
+});
+//adding a collection called tasks in a document with the given id 
+app.get("/:id",function(req,res){
+    let cityRef = db.collection('employees').doc(req.params.id);
+    let getDoc = cityRef.get()
+    .then(doc => {
+        if (!doc.exists) {
+        console.log('No such document!');
+        } else {
+        res.render("show",{data:doc.data()});
+        }
+    })
+    .catch(err => {
+        console.log('Error getting document', err);
+    });    
 
+});
+
+let doc=db.collection('employees').doc('QjAgkvWPSA0BHOlWmKRL');
+doc.collection("tasks").get().then( tasks =>{
+    tasks.forEach( item =>{
+        console.log(item.data());
+    });
+});
 
 
 
