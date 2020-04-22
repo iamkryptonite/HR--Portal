f1904b32809a9519680c46e39aa213ea039524c8
iamkryptonite <iamkryptonite@users.noreply.github.com>
2020-04-22 12:54:34 +0530

    added authentication routes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express       			=require('express'),
     app           			=express(),
     bodyParser              =require('body-parser');
 var firebase                = require('firebase/app');
+// var firebaseui              = require('firebaseui');
+// var ui                      = new firebaseui.auth.AuthUI(firebase.auth());
 require('firebase/auth');
 require('firebase/firestore');
 
@@ -23,38 +25,78 @@ var firebaseConfig = {
     appId: "1:735823522720:web:e9343d7b19d08d185692c5",
     measurementId: "G-0ML6E508G9"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+let auth=firebase.auth();
 let db = firebase.firestore();
+// var firebaseui              = require('firebaseui');
+// var ui                      = new firebaseui.auth.AuthUI(firebase.auth());
+
+// ui.start('#firebaseui-auth-container', {
+//     signInOptions: [
+//       firebase.auth.EmailAuthProvider.PROVIDER_ID
+//     ],
+//     // Other config options...
+//   });
 
 
 
 //=========================================================
+// app.get("/secret",function(req,res){
+//     res.render("addtasks");
+// });
+//=========================================Authentication Routes==========================================
+app.get("/login",function(req,res){
+    res.render("login");
+});
+app.post("/login",function(req,res){
+    const email=req.body.email;
+    const password=req.body.password;
+    auth.signInWithEmailAndPassword(email,password).then(cred=>{
+        console.log(cred.user);
+        res.redirect("/");        
+    });    
+});
+app.get("/signup",function(req,res){
+    res.render("signup");
+});
+app.post("/signup",function(req,res){
+    const name=req.body.name;
+    const email=req.body.email;
+    const password=req.body.password;
+    auth.createUserWithEmailAndPassword(email,password).then(cred=>{
+        res.redirect("/addEmployee",{email:email});
+    });
+});
+app.get("/logout",function(req,res){
+    auth.signOut().then(()=>{
+        res.redirect("/login");
+    });
+});
+//=========================================================================================================
 app.get("/",function(req,res){
     db.collection('employees').get()
     .then((snapshot) => {
-        // snapshot.forEach((doc) => {
-        // console.log(doc.id, '=>', doc.data());
-        // });
         res.render("home",{data:snapshot});
     })
     .catch((err) => {
         console.log('Error getting documents', err);
-    });
-    // res.render("home");
-})
+    });    
+});
+//==========================================================================================================
 //Adding employee to the employee collection
 app.get("/addEmployee",function(req,res){
-    // res.send("ssup");
     res.render("addEmp");
-
 });
 app.post("/addEmployee",function(req,res){
     var emp={
         name: req.body.name,
         Age: req.body.age,
         designation: req.body.designation,
-        gender: req.body.gender
+        gender: req.body.gender,
+        email: req.body.email,
+        password: req.body.password,
+        phone: req.body.phone
         }
         db.collection('employees').add(emp).then(ref=>{
             res.redirect("/");
@@ -81,9 +123,7 @@ app.get("/:id",function(req,res){
 });
 //add tasks to tasks subcollection of an employee
 app.get("/:id/addtasks",function(req,res){
-    res.render("addTasks",{id:req.params.id});
-    // let tasks=db.collection('employees').doc(req.params.id).collection('tasks').doc()
-
+    res.render("secret",{id: req.params.id});
 });
 app.post("/:id/addtasks",function(req,res){
     //res.render("addTasks",{id:req.params.id});
@@ -95,7 +135,6 @@ app.post("/:id/addtasks",function(req,res){
     }
     db.collection('employees').doc(req.params.id).collection('tasks').add(task).then(ref=>{
         console.log(req.params.id);
-        //var i=req.params.id;
         res.redirect("/"+req.params.id);
     });
 });
@@ -108,20 +147,7 @@ app.post("/:id/addtasks",function(req,res){
 // });
 
 
-// wdgkdjksjxaxascasaasschdka
-
-
-
-
-
-
-
-
-
-
-
-
-
+// wdgkdjksjxaxascasaasscwcqqhdka
 //==============================================================
 app.listen(3000,function(){
 	console.log("server is live");
